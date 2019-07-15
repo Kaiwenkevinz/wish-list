b4d1003edf7cdfeecd64497d091fea9178d129fd
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2019-07-15 15:44:00 -0600

    change debug to false

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x&*p*1fo(in@(@s(*58jt_ifhi!+cr*o2z@zf0vhdh^0_6+#2j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
