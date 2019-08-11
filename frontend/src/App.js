8cd5fe62e9d692e53089f0bf8d3fb22f716ecaf9
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2019-08-11 14:54:43 -0600

    backup for ui inhencement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ import Header from './components/layouts/Header';
 import Register from './components/accounts/Register';
 import Login from './components/accounts/Login';
 
-
 const alertOptions = {
   // you can also just use 'bottom center'
   position: 'top center',
