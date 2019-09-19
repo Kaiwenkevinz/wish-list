2ab09eecb14731bd4e0e09439cd8f9c22f73ffa0
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2019-09-19 10:34:25 -0600

    Updates

---
+++ precache-manifest.8848c8c763edb07b9103cfccdb036864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6201c4505d35034c5e27200c948ff016",
+    "url": "/wish-list/index.html"
+  },
+  {
+    "revision": "90af747abeaf57d49f19",
+    "url": "/wish-list/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "689e4422f6cf1d25c0c0",
+    "url": "/wish-list/static/css/main.a5fe8c4c.chunk.css"
+  },
+  {
+    "revision": "90af747abeaf57d49f19",
+    "url": "/wish-list/static/js/2.59f6cc92.chunk.js"
+  },
+  {
+    "revision": "689e4422f6cf1d25c0c0",
+    "url": "/wish-list/static/js/main.63ae2a83.chunk.js"
+  },
+  {
+    "revision": "01dfbde12d0081f886a5",
+    "url": "/wish-list/static/js/runtime~main.2c259962.js"
+  }
+]);
